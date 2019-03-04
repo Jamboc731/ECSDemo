81c3f177b506becd72edaf8633796945f750fb2e
Jamboc731 <jamiec.covellj@gmail.com>
2019-03-04 08:52:28 +0000

    Added random of 4 mats

--- Assets/Scripts/Components/CSpawning.cs
+++ Assets/Scripts/Components/CSpawning.cs
@@ -8,12 +8,12 @@ using Unity.Transforms;
 public class CSpawning : MonoBehaviour
 {
 
-    [SerializeField] GameObject unitPrefab;
+    [SerializeField] GameObject[] unitPrefabs;
     Vector3 [] spawns = new Vector3 [] { new Vector3 (0, 0, -3), new Vector3 (0, 0, -1.5f), new Vector3 (0, 0, 0), new Vector3 (0, 0, 1.5f), new Vector3 (0, 0, 3), new Vector3 (-3, 0, -3), new Vector3 (-3, 0, -1.5f), new Vector3 (-3, 0, 0), new Vector3 (-3, 0, 1.5f), new Vector3 (-3, 0, 3), new Vector3 (-1.5f, 0, -3), new Vector3 (-1.5f, 0, -1.5f), new Vector3 (-1.5f, 0, 0), new Vector3 (-1.5f, 0, 1.5f), new Vector3 (-1.5f, 0, 3), new Vector3 (1.5f, 0, -3), new Vector3 (1.5f, 0, -1.5f), new Vector3 (1.5f, 0, 0), new Vector3 (1.5f, 0, 1.5f), new Vector3 (1.5f, 0, 3), new Vector3 (3, 0, -3), new Vector3 (3, 0, -1.5f), new Vector3 (3, 0, 0), new Vector3 (3, 0, 1.5f), new Vector3 (3, 0, 3) };
 
     public void SpawnCube (Vector3 position)
     {
-        Instantiate (unitPrefab, transform.position + position, Quaternion.identity);
+        Instantiate (unitPrefabs[Random.Range(0, 4)], transform.position + position, Quaternion.identity);
     }
 
     public Vector3[] GetSpawns ()
